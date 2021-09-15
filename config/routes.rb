86e852566890b08a6debfc36f3c0e90daf421c24
Taiga Watanabe <64018398+TaigaWatanabe@users.noreply.github.com>
2021-09-15 20:48:56 +0900

    layout new property page (#7)
    * layout new property page
    
    * fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do  
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
   root 'pages#home' 
   resources :users, only: :show
+  resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
