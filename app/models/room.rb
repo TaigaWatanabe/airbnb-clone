15d0ee056f0db7d3716ac34084d80c238805e769
Taiga <1208taiga@gmail.com>
2021-09-28 20:05:08 +0900

    create layout-of-profile-page

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,4 +6,25 @@ class Room < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+
+  def average_rating
+    ids = reservations.pluck(:user_id) # pluck all the user_id from the reservations
+
+    # add a condition to return 0 if the reviews from guests is 0
+    if reviews.where(user_id: ids).any?
+      reviews.where(user_id: ids).average(:rating).round(2).to_i
+    else
+      return 0
+    end
+  end
+
+  def guest_reviews
+    ids = reservations.pluck(:user_id)
+    reviews.where(user_id: ids)
+  end
+
+  def host_reviews
+    ids = reservations.pluck(:user_id)
+    reviews.where.not(user_id: ids)
+  end
 end
