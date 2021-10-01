d4c5b91bc33b33bb7cb0aa9313fd7493bdb520f9
Taiga <1208taiga@gmail.com>
2021-10-01 21:43:33 +0900

    fixed host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3002 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+    :password => Rails.application.credentials.sendgrid[:api_key], # This is the secret sendgrid API key which was issued during API key creation
+    :domain => 'http://localhost:3002',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://airbnb-taiga.herokuapp.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+    :password => Rails.application.credentials.sendgrid[:api_key], # This is the secret sendgrid API key which was issued during API key creation
+    :domain => 'https://airbnb-taiga.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
